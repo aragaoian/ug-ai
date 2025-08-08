19c6a145006fe9ff7aede37a2112ee52eabe2ca8
Ian C Aragao <ianaragaobusiness@gmail.com>
2025-08-08 00:51:49 -0300

    Using more than 1 documents for knowledge base; Tweaking chunking parameters to get better performance (500-1000 words have been the most sucessful so far)

--- src/main.py
+++ src/main.py
@@ -6,47 +6,74 @@ from langchain_core.prompts import ChatPromptTemplate
 from langchain_core.messages import HumanMessage, AIMessage
 from langchain_ollama import OllamaEmbeddings
 from langchain_ollama import OllamaLLM
+import langchain
+from langchain_text_splitters import RecursiveCharacterTextSplitter
+
+langchain.verbose = True
+langchain.debug = True
 
 """
     TODO
-    * Try with more than 1 pdf
-    * try with larger pdf's
+    * Try with more than 1 pdf []
+    * try with larger pdf's []
 """
 
-DIRECTORY = "/home/ian/Univali/ug_ai/test.pdf"
+DIRECTORY = "/home/ian/Univali/ug_ai/docs"
 SYSTEM_PROMPT = """
-    You are an AI assistant responsible for answering questions about 
-    a university program called 'Programa Universidade Gratuita'.
-    It is a program from the state of Santa Catarina for graduation studentes.
+    You are a virtual assistant specialized in answering questions and guiding students 
+    about the Programa Universidade Gratuita, created by the State of Santa Catarina for 
+    undergraduate students.
 
-    **INSTRUCTIONS**
-    1. For all questions asked you must respond in BRAZILIAN PORTUGUESE;
-    2. Answers must be short and concise;
-    3. For all questions aked outsite of the Programa Universidade Gratuita scope or related must 
-    be respondend with a default message, saying that it can't respond too out of scope questions;
-    4. Any form of hate speech (racism, nazism, homophobia, etc) must be respondend with the same message
-    for item 3;
+    Response Rules:
+    1. Language: Always respond in Brazilian Portuguese.
+    2. Clarity and Objectivity:
+        - Be clear and direct.
+        - Provide all relevant details when necessary.
+        - If the question allows, respond briefly and concisely.
+    3. Scope:
+        - Answer only questions related to the Programa Universidade Gratuita, including:
+        - Eligibility requirements (e.g., residency time in Santa Catarina, income limits, 
+        academic performance).
+        - Application process and deadlines.
+        - Courses, universities, and benefits covered.
+        - Obligations after receiving the scholarship.
+        - If the question covers these topics, even without explicitly mentioning the program name, 
+        consider it within scope.
+        - For questions outside the scope, respond exactly with:
+        "Desculpe, não posso responder perguntas fora do escopo do Programa Universidade Gratuita."
+    4. Sensitive Content:
+        - Any form of hate speech (racism, Nazism, homophobia, etc.) will not be tolerated.
+        - For such cases, use the same default response from item 3.
+    5. Additional Context:
+        - Use previous conversation history to maintain coherence: {history}
+        - Use the provided document content as the source of information: {context}
 
-    Previous conversations:
-    {history}
-
-    Document context:
-    {context}
 """
+loaded_raw_docs = []
+for file_name in os.listdir(DIRECTORY):
+    file_path = os.path.join(DIRECTORY, file_name)
+    loader = PyPDFLoader(file_path)
+    loaded_file = loader.load()
+    loaded_raw_docs.extend(loaded_file)  # load file into list
+    print(f"{file_name}: {len(loaded_file)}")
 
-# Load pdf file
-loader = PyPDFLoader(DIRECTORY)
-loaded_doc = loader.load()
+text_splitter = RecursiveCharacterTextSplitter(
+    chunk_size=1000, chunk_overlap=50, length_function=len, is_separator_regex=False
+)
+
+documents = text_splitter.split_documents(loaded_raw_docs)
+
 
 # Load embeddings model
 embeddings = OllamaEmbeddings(model="all-minilm")
 
+
 # Create db
-if os.path.exists("chroma_db"):
+if os.path.exists("/src/chroma_db"):
     vectorstore = Chroma(embedding_function=embeddings, persist_directory="chroma_db")
 else:
     vectorstore = Chroma.from_documents(
-        loaded_doc, embeddings, persist_directory="chroma_db"
+        documents, embeddings, persist_directory="chroma_db"
     )
 
 llm = OllamaLLM(model="llama3.2")
@@ -59,20 +86,28 @@ qa_prompt = ChatPromptTemplate.from_messages(
 )
 
 
+def sanitize_prompt(prompt):
+    # Here is your sanitizing service
+    print(prompt)
+    return prompt
+
+
 def chainingFunction():
-    retriever = vectorstore.as_retriever()
+    retriever = vectorstore.as_retriever(
+        search_type="similarity_score_threshold",
+        search_kwargs={"score_threshold": 0.5, "k": 4},
+    )
     history = []
 
     while True:
         query = input("User:")
-
-        history.append({"role": "user", "content": HumanMessage(content=query)})
 
         if query:
             relevant_docs = retriever.invoke(query)
             context_documents_str = "\n\n".join(
                 doc.page_content for doc in relevant_docs
             )
+            history.append({"role": "user", "content": HumanMessage(content=query)})
         else:
             context_documents_str = ""
 
@@ -80,7 +115,9 @@ def chainingFunction():
             history=history, context=context_documents_str
         )
 
-        llm_chain = {"input": RunnablePassthrough()} | qa_prompt_local | llm
+        llm_chain = (
+            {"input": RunnablePassthrough()} | qa_prompt_local | sanitize_prompt | llm
+        )
 
         result = llm_chain.invoke(query)
 
